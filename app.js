9bb0318cfb403e02c649c8a5d038713e393a645e
Q. Price <priceqp@gmail.com>
2019-05-29 18:57:45 -0500

    Setup and test routes

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+// G E T    R O U T E S
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/booking", function(req, res){
+    res.render("booking");
+});
+
+app.get("/courses", function(req, res){
+    res.render("courses");
+});
+
+app.get("/email", function(req, res){
+    res.render("email");
+});
 
 
 
